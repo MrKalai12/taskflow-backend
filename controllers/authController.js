9779b96adad37cd31c84e9e7c5feb6230b49abfb
MrKalai12 <kalaicreater12@gmail.com>
2026-03-16 19:27:54 +0530

    added: getme/authController

--- controllers/authController.js
+++ controllers/authController.js
@@ -208,4 +208,26 @@ const refresh = async (req, res) => {
     });
   }
 };
-module.exports = { signup, login, logout, refresh };
+
+const getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.userId)
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    return res.status(200).json({
+      user: {
+        id: user._id,
+        userName: user.userName,
+        email: user.email,
+        role: user.role,
+      }
+    })
+  } catch (error) {
+    return res.status(500).json({ message: 'Server Error' })
+  }
+}
+
+module.exports = { signup, login, logout, refresh, getMe }
+
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { signup, login, logout, refresh} = require('../controllers/authController');
+const { signup, login, logout, refresh, getMe} = require('../controllers/authController');
 
 router.post('/signup', signup);
 router.post('/login', login);
 router.post('/refresh', refresh);
 router.post('/logout', logout);
+router.get('/me', authMiddleware, getMe);
 
 module.exports = router; 
